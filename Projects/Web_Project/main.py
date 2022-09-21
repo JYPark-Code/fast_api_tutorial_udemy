fab6915ea8df7c768ae90326edac8e48b87a6d78
Ji Yong Park <toboy93@gmail.com>
2022-09-21 15:10:49 +0900

    220921 Web Project 마무리 - login/edit PW

--- Projects/Web_Project/main.py
+++ Projects/Web_Project/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Depends
 import models
 from database import engine
-from routers import auth, todos
+from routers import auth, todos, users
 from starlette.staticfiles import StaticFiles
-
+from starlette import status
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -11,6 +12,12 @@ models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(users.router)
 
